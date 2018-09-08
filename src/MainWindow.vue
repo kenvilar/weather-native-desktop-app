57e6a0f57fa851cb1b798649588c22207aa7cdd4
Ken Vilar <kenvilar@gmail.com>
2018-09-08 23:47:53 +0800

    Create desktop window

---
+++ src/MainWindow.vue
@@ -0,0 +1,17 @@
+<template>
+  <Window title="weather-native-desktop-app" width="400" height="150" margined v-on:close="exit">
+    <Box>
+      <Text>Ken Vilar's Weather Native Desktop Application</Text>
+    </Box>
+  </Window>
+</template>
+
+<script>
+export default {
+  methods: {
+    exit() {
+      this.$exit();
+    }
+  }
+}
+</script>
