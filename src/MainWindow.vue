d6ea3ae7c965ca4e5657c8f8eefb6186774cdcb0
Ken Vilar <kenvilar@gmail.com>
2018-09-09 00:38:20 +0800

    Modify GUI

--- src/MainWindow.vue
+++ src/MainWindow.vue
@@ -1,10 +1,29 @@
 <template>
-  <Window title="weather-native-desktop-app" width="400" height="150" margined v-on:close="exit">
+  <Window title="Ken Vilar Weather App" width="400" height="150" margined v-on:close="exit">
     <Box padded>
       <Box horizontal padded>
         <TextInput v-model="query" stretchy></TextInput>
-        <Button !enabled="!!query">Search</Button>
+        <Button !enabled="!!query" @click="showMyWeather" placeholder="Search a city...">Search</Button>
       </Box>
+      <Separator horizontal />
+      <Group margined>
+        <Box padded>
+          <Text v-if="error">There is no such city in the database</Text>
+          <Box v-if="!!city">
+            <Box padded horizontal>
+              <Text stretchy>{{ kencity }}, {{ kencountry }}</Text>
+              <Text>{{ kentemp }}&deg;C</Text>
+            </Box>
+            <Text>{{ kenweatherDescription }}</Text>
+            <Separator horizontal />
+            <Box padded horizontal>
+              <Text stretchy>Min: {{ _tempMin }}&deg;C</Text>
+              <Text stretchy>Max: {{ _tempMax }}&deg;C</Text>
+              <Text stretchy>Humidity: {{ _humidity }}%</Text>
+            </Box>
+          </Box>
+        </Box>
+      </Group>
     </Box>
   </Window>
 </template>
@@ -15,34 +34,34 @@ export default {
     return {
       query: '',
       error: false,
-      city: '',
-      country: '',
-      weatherDescription: '',
-      temp: null,
-      tempMin: null,
-      tempMax: null,
-      humidity: null,
+      kencity: '',
+      kencountry: '',
+      kenweatherDescription: '',
+      kentemp: null,
+      _tempMin: null,
+      _tempMax: null,
+      _humidity: null,
     };
   },
   methods: {
     exit() {
       this.$exit();
     },
-    showWeather() {
+    showMyWeather() {
       axios.get(
-      	`/weather?q=${this.query}&units=metric&&appid=${apiKey}`,
+      	`/weather?q=${this.query}&units=metric&&appid=${_apiKey}`,
       ).then(response => {
-        this.city = response.data.name;
-        this.country = response.data.sys.country;
-        this.weatherDescription = response.data.weather[0].description;
-        this.temp = response.data.main.temp;
-        this.tempMin = response.data.main.temp_min;
-        this.tempMax = response.data.main.temp_max;
-        this.humidity = response.data.main.humidity;
+        this.kencity = response.data.name;
+        this.kencountry = response.data.sys.country;
+        this.kenweatherDescription = response.data.weather[0].description;
+        this.kentemp = response.data.main.temp;
+        this._tempMin = response.data.main.temp_min;
+        this._tempMax = response.data.main.temp_max;
+        this._humidity = response.data.main.humidity;
         this.error = false;
       }).catch(() => {
         this.error = true;
-        this.city = '';
+        this.kencity = '';
       });
     },
   }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vuido'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://api.openweathermap.org/data/2.5';
-const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+const _apiKey = process.env.OPENWEATHERMAP_API_KEY;
 
 import MainWindow from './MainWindow'
 
